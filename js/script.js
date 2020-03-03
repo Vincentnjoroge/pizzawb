c0aef71d85eb9846bb3240d5977f63d8f51c3a85
vincent njoroge <email@example.com>
2020-03-03 14:32:18 +0300

    updated order.styles

--- js/script.js
+++ js/script.js
@@ -1,105 +1,100 @@
-// Business Logic 🎪
-function Pizza () {
-    this.toppings = [];
-    this.size;
-    this.total;
-   }
-  Pizza.prototype.cost = function () {
-    var cost = 0;
-    this.toppings.map(function(topping) {
-      cost += topping
-    });
-    var total = cost + this.size;
-    this.total = "Your total is: " + "$" + total.toFixed(2);
+// Business Logic
+  function Pizza () {
+  this.toppings = [];
+  this.size;
+  this.total;
+  }
+ Pizza.prototype.cost = function () {
+  var cost = 0;
+  this.toppings.map(function(topping) {
+    cost += topping
+  });
+  var total = cost + this.size;
+  this.total = "Your total is: " + "ksh" + total.toFixed(2);
   };
-  
-  // Uil 😃 
-  $(function() {
-    $("#pizzaOrder").submit(function(event) {
-      event.preventDefault();
-      var pizza = new Pizza();
-      $("#pizzaForm").fadeIn();
-      $("#orderStarter button").hide();
-      $(".pizza-image-container").fadeIn();
-      $("button#addToppings").click(function() {
-        var toppingsPrice = parseFloat($("#toppings").val());
-        var toppingsText = $("#toppings option:selected").text();
-        if (toppingsPrice === 0) {
-          $(".error").show();
-          $("#toppings").addClass('error-border');
-        } else if(toppingsPrice === 0.5) {
+
+// User Interface Logic
+  (function () {
+   $("#pizzaOrder").submit(function(event) {
+   event.preventDefault();
+    var pizza = new Pizza();
+    $("#pizzaForm").fadeIn();
+    $("#orderStarter button").hide();
+    $(".pizza-image-container").fadeIn();
+    $("button#addToppings").click(function() {
+      var toppingsPrice = parseFloat($("#toppings").val());
+      var toppingsText = $("#toppings option:selected").text();
+      if (toppingsPrice === 0) {
+        $(".error").show();
+        $("#toppings").addClass('error-border');
+      } else if(toppingsPrice === 0.5) {
+        $(".order-receipt").fadeIn();
+        $(".error").hide();
+        $("#toppings").removeClass('error-border');
+        pizza.toppings.push(toppingsPrice);
+        $("#orderConfirmation ul").append('<li>' + toppingsText + '&nbsp;</li>');
+        if(toppingsText === "Pepperoni") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/pepperonis.png">');
+        } else if(toppingsText === "Sausage") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/sausages.png">');
+        } else if(toppingsText === "Bell Pepper") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/bell-pepper.png">');
+        } else if(toppingsText === "Mushroom") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/mushroom.png">');
+        } else if(toppingsText === "Onion") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/onions.png">');
+        } else if(toppingsText === "Artichoke") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/artichokes.png">');
+        } else if(toppingsText === "Tempeh") {
+          $(".pizza-toppings-container img").remove();
+          $(".pizza-toppings-container").prepend('<img src="imgs/tempehs.png">');
+        }
+        $("#toppings").val(0);
+      }
+    });
+    $("#order").click(function() {
+      var size = parseInt($("#sizes").val());
+      if(size === 0) {
+        $(".error").show();
+      } else {
+        $(".error").hide();
+        $("#pizzaForm").fadeOut();
+        $(".pizza-image-container").fadeOut();
+        $(".order-receipt").fadeOut();
+        setInterval(function () {
           $(".order-receipt").fadeIn();
-          $(".error").hide();
-          $("#toppings").removeClass('error-border');
-          pizza.toppings.push(toppingsPrice);
-          $("#orderConfirmation ul").append('<li>' + toppingsText + '&nbsp;</li>');
-          if(toppingsText === "Pepperoni") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/pepperonis.png">');
-          } else if(toppingsText === "Sausage") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/sausages.png">');
-          } else if(toppingsText === "Bell Pepper") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/bell-pepper.png">');
-          } else if(toppingsText === "Mushroom") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/mushroom.png">');
-          } else if(toppingsText === "Onion") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/onions.png">');
-          } else if(toppingsText === "Artichoke") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/artichokes.png">');
-          } else if(toppingsText === "Tempeh") {
-            $(".pizza-toppings-container img").remove();
-            $(".pizza-toppings-container").prepend('<img src="imgs/tempehs.png">');
-          }
-          $("#toppings").val(0);
-        }
-      });
-      $("#order").click(function() {
-        var size = parseInt($("#sizes").val());
-        if(size === 0) {
-          $(".error").show();
-        } else {
-          $(".error").hide();
-          $("#pizzaForm").fadeOut();
-          $(".pizza-image-container").fadeOut();
-          $(".order-receipt").fadeOut();
-          setInterval(function () {
-            $(".order-receipt").fadeIn();
-            $(".order-receipt ul").addClass('display-block');
-            $(".order-receipt").addClass('text-center');
-            $("#totalCost").text(pizza.total);
-            $("#totalCost").fadeIn();
-          }, 700);
-          pizza.size = size;
-          pizza.cost();
-        
-             // Get the video📼
-           var video = document.getElementById("myVideo");
-  
-           // Get the button
-           var btn = document.getElementById("myBtn");
-  
-              // Pause and play the video, and change the button text
-                function myFunction() {
-                if (video.paused) {
-                video.play();
-             btn.innerHTML = "Pause";
-           } else {
-            video.pause();
-             btn.innerHTML = "Play";
-          }
-        }
-  
+          $(".order-receipt ul").addClass('display-block');
+          $(".order-receipt").addClass('text-center');
+          $("#totalCost").text(pizza.total);
+          $("#totalCost").fadeIn();
+        }, 700);
+        pizza.size = size;
+        pizza.cost();
       }
     });
   });
-});      
+});
+
+        
+// Get the video📼
+var video = document.getElementById("myVideo");
+// Get the button
+var btn = document.getElementById("myBtn");
+// Pause and play the video, and change the button text
+ function myFunction() {
+ if (video.paused) {
+ video.play();
+btn.innerHTML = "Pause";
+ } else {
+ video.pause();
+ btn.innerHTML = "Play";
+ }
+}
   
-
-
-
   
